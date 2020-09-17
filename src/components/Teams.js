b6d758df26fadd1bc00fbdbf71e7451133a06ba6
suraj kamal.shaik <surajsheru@gmail.com>
2020-09-17 19:50:31 +0530

    second

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 // const xlsxFile = require('read-excel-file/node');
 // const second=require('read-excel-file/node');
 // const sh=require('read-excel-file/node');
@@ -9,26 +9,28 @@ import Fload from './Fload';
 
 function Teams() {
     let main=[],test=[],result=[],show=[];
-    let difference=[];
+    let difference=[],di=[];
     let i,shdate;
+    const[showabs,setShowabs]=useState([])
+    const [showpre,setShowpre]=useState([])
     useEffect(()=>{
 
 console.log("welcome")
 
 const input = document.getElementById('input');
 input.addEventListener("change",()=>{
-    sh((input.files[0]), { getSheets: true }).then((sheets) => {
-        sheets.forEach((obj)=>{
+    //sh((input.files[0]), { getSheets: true }).then((sheets) => {
+      //  sheets.forEach((obj)=>{
          
-             console.log(obj.name);
-             shdate=obj.name;
+        //     console.log(obj.name);
+          //   shdate=obj.name;
              readXlsxFile((input.files[0]), { sheet: 'cse' }).then((rows) => {
       
-                second((input.files[0]), { sheet: obj.name }).then((cols)=>{
+                second((input.files[0]), { sheet: 'today' }).then((cols)=>{
                   //console.log(cols);
                   //console.log(rows);
                   main=rows;
-                 // console.log(cols)
+                 console.log(cols)
                   for(i=0;i<cols.length;i++)
                   {
                       if(i===0)
@@ -41,11 +43,11 @@ input.addEventListener("change",()=>{
                       }
                   }
                   //test=cols;
-                   //console.log(test);
+                   console.log(test);
                    show=rows;
                        
-                  console.log("date"+""+obj.name);
-                result.push("date"+""+obj.name);
+                 // console.log("date"+""+obj.name);
+                //result.push("date"+""+obj.name);
                   //result=[];
                   //document.writeln("date"+""+obj.name)
                   test.forEach(element => {
@@ -57,15 +59,19 @@ input.addEventListener("change",()=>{
                               console.table(main[i][0])
                              //document.writeln(main[i][0])
                              result.push(main[i][0])
-                           //  console.log(result)
+                           console.log(result)
                           }
                           
                          }
-                      });  })  })          
+                      });  
+                    }) 
+                 })          
          })
-     })
+     },[])
     
-}) },[])
+//}) 
+//},
+//[])
      const ReptileListItems=()=> {
         
      let ronumbers=[];
@@ -76,32 +82,43 @@ input.addEventListener("change",()=>{
   
      result=result.sort();
      difference = ronumbers.filter(x => !result.includes(x));
-     document.writeln("<h1>Present  numbers:"+shdate+"</h1>")
+     di=difference;
+     //document.writeln("<h1>Present  numbers:"+shdate+"</h1>")
     
      result.map((reptile) => {
             
-        document.writeln(reptile+",\n")
-        document.write('\n');
+       // document.writeln(reptile+",\n")
+       // document.write('\n');
     });
-     document.writeln("<h1>Absenties numbers:"+shdate+"</h1>")
+    //document.writeln("<h1>Absenties numbers:"+shdate+"</h1>")
      console.log("abs"+difference)
      difference.map((reptile) => {
             
-            document.writeln(reptile+",\n")
-            document.write('\n');
+           //document.writeln(reptile+",\n")
+          //document.write('\n');
         });
- 
-         
+  
+        setShowabs(difference); 
+        setShowpre(result);
      };
      return (
-        <div>
+        
+        <div style={{float:"inherit"}}>
         <div>
              <h1>Upload FILE </h1>
             <input type="file" id="input" />
             <button onClick={ReptileListItems}>CLICK FOR Absent Numbers</button>
       </div>
-                  
-               
+            <table  border='1' >     <tr>
+              <td>Present</td><td>Absent</td></tr><tr>
+             <td><table>
+           {
+
+            showpre.map((item)=><tr key={item}><td>{item}</td></tr>)}</table></td>
+            <td><table style={{textAlign:"top"}}>
+            {   
+            showabs.map((item)=><tr key={item}><td>{item}</td></tr>)
+            }</table></td></tr></table>
         </div>
     )
 }
